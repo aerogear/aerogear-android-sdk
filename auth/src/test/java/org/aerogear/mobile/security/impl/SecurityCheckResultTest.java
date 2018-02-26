106a03c85a337bef8aa0503261dae07d9471e986
Massimiliano Ziccardi <mziccard@redhat.com>
2018-02-26 11:54:27 +0100

    Updated javadocs

--- auth/src/test/java/org/aerogear/mobile/security/impl/SecurityCheckResultTest.java
+++ auth/src/test/java/org/aerogear/mobile/security/impl/SecurityCheckResultTest.java
@@ -5,18 +5,15 @@ import org.aerogear.mobile.security.SecurityCheckResult;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
-import static org.mockito.Mockito.*;
 
 public class SecurityCheckResultTest {
 
     SecurityCheckResult result;
-    static final String RESULT_NAME = "testResult";
     static final boolean RESULT_PASSED = true;
 
     @Mock
@@ -25,7 +22,6 @@ public class SecurityCheckResultTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        //when(securityCheck.getName()).thenReturn()
 
         result = new SecurityCheckResultImpl(securityCheck, RESULT_PASSED);
     }
