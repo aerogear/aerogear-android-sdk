d5aa60318a69583f74813764847510d605e910ac
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-08 09:33:43 +0100

    removes nonexistent test-runner

--- core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
+++ core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(JUnit4.class)
 public class MetricsServiceIntegrationTest {
 
     @Before
