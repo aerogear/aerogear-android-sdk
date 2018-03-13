5d2e3fed80e241696a9a9ec67aaace13b430c03e
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-03-13 12:59:02 +0000

    added certpinner

---
+++ core/src/main/java/org/aerogear/mobile/core/configuration/HttpsConfiguration.java
@@ -0,0 +1,46 @@
+package org.aerogear.mobile.core.configuration;
+
+/**
+ * This represents a parsed Https configuration from JSON configuration
+ */
+
+public class HttpsConfiguration {
+    private final String hostName;
+    private final String certificateHash;
+
+    private HttpsConfiguration(final String hostName, final String certificateHash){
+        this.hostName = hostName;
+        this.certificateHash = certificateHash;
+    }
+
+    public static class Builder {
+        protected String hostName;
+        protected String certificateHash;
+
+        public Builder setHostName(final String hostName){
+            this.hostName = hostName;
+            return this;
+        }
+
+        public Builder setCertificateHash(final String certificateHash){
+            this.certificateHash = certificateHash;
+            return this;
+        }
+
+        public HttpsConfiguration build(){
+            return new HttpsConfiguration(this.hostName, this.certificateHash);
+        }
+    }
+
+    public String getHostName(){
+        return hostName;
+    }
+
+    public String getCertificateHash(){
+        return certificateHash;
+    }
+
+    public static Builder newHashConfiguration() {
+        return new Builder();
+    }
+}
