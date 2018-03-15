ef9d9fe04d07aedad1a77845757d8025cdae8b8c
Daniel Passos <daniel@passos.me>
2018-03-15 09:56:56 -0300

    Update logger to use Throwable instead of Exception

--- core/src/main/java/org/aerogear/mobile/core/logging/Logger.java
+++ core/src/main/java/org/aerogear/mobile/core/logging/Logger.java
@@ -6,32 +6,32 @@ public interface Logger {
 
     void info(String message);
 
-    void info(String tag, String message, Exception e);
+    void info(String tag, String message, Throwable e);
 
-    void info(String message, Exception e);
+    void info(String message, Throwable e);
 
     void warning(String tag, String message);
 
     void warning(String message);
 
-    void warning(String tag, String message, Exception e);
+    void warning(String tag, String message, Throwable e);
 
-    void warning(String message, Exception e);
+    void warning(String message, Throwable e);
 
     void debug(String tag, String message);
 
     void debug(String message);
 
-    void debug(String tag, String message, Exception e);
+    void debug(String tag, String message, Throwable e);
 
-    void debug(String message, Exception e);
+    void debug(String message, Throwable e);
 
     void error(String tag, String message);
 
     void error(String message);
 
-    void error(String tag, String message, Exception e);
+    void error(String tag, String message, Throwable e);
 
-    void error(String message, Exception e);
+    void error(String message, Throwable e);
 
 }
--- core/src/main/java/org/aerogear/mobile/core/logging/LoggerAdapter.java
+++ core/src/main/java/org/aerogear/mobile/core/logging/LoggerAdapter.java
@@ -17,12 +17,12 @@ public class LoggerAdapter implements Logger {
     }
 
     @Override
-    public void info(String tag, String message, Exception e) {
+    public void info(String tag, String message, Throwable e) {
         Log.i(tag, message, e);
     }
 
     @Override
-    public void info(String message, Exception e) {
+    public void info(String message, Throwable e) {
         Log.i(TAG, message, e);
     }
 
@@ -37,12 +37,12 @@ public class LoggerAdapter implements Logger {
     }
 
     @Override
-    public void warning(String tag, String message, Exception e) {
+    public void warning(String tag, String message, Throwable e) {
         Log.w(tag, message, e);
     }
 
     @Override
-    public void warning(String message, Exception e) {
+    public void warning(String message, Throwable e) {
         Log.w(TAG, message, e);
     }
 
@@ -57,12 +57,12 @@ public class LoggerAdapter implements Logger {
     }
 
     @Override
-    public void debug(String tag, String message, Exception e) {
+    public void debug(String tag, String message, Throwable e) {
         Log.d(tag, message, e);
     }
 
     @Override
-    public void debug(String message, Exception e) {
+    public void debug(String message, Throwable e) {
         Log.d(TAG, message, e);
     }
 
@@ -77,12 +77,12 @@ public class LoggerAdapter implements Logger {
     }
 
     @Override
-    public void error(String tag, String message, Exception e) {
+    public void error(String tag, String message, Throwable e) {
         Log.e(tag, message, e);
     }
 
     @Override
-    public void error(String message, Exception e) {
+    public void error(String message, Throwable e) {
         Log.e(TAG, message, e);
     }
 
