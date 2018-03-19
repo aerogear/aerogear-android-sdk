af32b0d1a733c8d6b3f3f2ec4cbbd0b03d8dead1
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-19 16:12:13 +0100

    boilerplate integration test

--- core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
+++ core/src/test/java/org/aerogear/mobile/core/integration/MetricsServiceIntegrationTest.java
@@ -1,23 +1,50 @@
 package org.aerogear.mobile.core.integration;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.robolectric.RuntimeEnvironment;
+
+import org.aerogear.mobile.core.Callback;
+import org.aerogear.mobile.core.MobileCore;
+import org.aerogear.mobile.core.configuration.ServiceConfiguration;
+import org.aerogear.mobile.core.metrics.MetricsService;
 
 @RunWith(JUnit4.class)
 public class MetricsServiceIntegrationTest {
 
+    MobileCore mobileCore;
+    MetricsService metricsService;
+
     @Before
     public void setUp() throws Exception {
-
+        mobileCore = MobileCore.init(RuntimeEnvironment.application);
+        metricsService = new MetricsService();
     }
 
     @Test
     public void testSendMetricsShouldReturnNoError() throws Exception {
-        fail("Not yet implemented");
+        ServiceConfiguration serviceConfiguration =
+                        new ServiceConfiguration.Builder().setUrl("TODO put integration server URL").build();
+        metricsService.configure(mobileCore, serviceConfiguration);
+
+        final Callback testCallback = new Callback() {
+            @Override
+            public void onSuccess() {
+                assertTrue(true);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                fail("Should not throw an error: " + error.getMessage());
+            }
+        };
+
+        metricsService.sendAppAndDeviceMetrics(testCallback);
     }
 
 }
