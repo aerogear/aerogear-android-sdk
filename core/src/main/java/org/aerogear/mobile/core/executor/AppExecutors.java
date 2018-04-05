bb4eb6e9f23cb14ac1610880cb2d6ef75e50eff5
Hoyt Summers Pittman <secondsun@gmail.com>
2018-04-05 12:44:40 -0400

    Fixing Executor for Android main thread

--- core/src/main/java/org/aerogear/mobile/core/executor/AppExecutors.java
+++ core/src/main/java/org/aerogear/mobile/core/executor/AppExecutors.java
@@ -1,9 +1,13 @@
 package org.aerogear.mobile.core.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 import android.os.Handler;
 import android.os.Looper;
@@ -17,12 +21,45 @@ public final class AppExecutors {
 
     private static final ExecutorService networkExecutor = Executors.newSingleThreadExecutor();
 
-    private static final ExecutorService mainThreadExecutor = Executors.newSingleThreadExecutor(new ThreadFactory() {
+    private static final ExecutorService mainThreadExecutor = new AbstractExecutorService() {
+        private final Handler handler = new Handler(Looper.getMainLooper());
         @Override
-        public Thread newThread(@NonNull Runnable r) {
-            return Looper.getMainLooper().getThread();
+        public void shutdown() {
         }
-    });
+
+        @NonNull
+        @Override
+        public List<Runnable> shutdownNow() {
+            return new ArrayList<>();
+        }
+
+        @Override
+        public boolean isShutdown() {
+            return false;
+        }
+
+        @Override
+        public boolean isTerminated() {
+            return false;
+        }
+
+        @Override
+        public boolean awaitTermination(long l, @NonNull TimeUnit timeUnit) throws InterruptedException {
+            return false;
+        }
+
+        @Override
+        public void execute(@NonNull Runnable runnable) {
+            handler.post(runnable);
+        }
+    };
+
+//        Executors.newSingleThreadExecutor(new ThreadFactory() {
+//        @Override
+//        public Thread newThread(@NonNull Runnable r) {
+//            return Looper.getMainLooper().getThread();
+//        }
+//    });
 
 
     private static final ExecutorService serviceThreadExecutor =
