cecf5a9cec99decd32155e29da47388c2342d725
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-04-06 09:13:19 +0100

    dialog added

--- example/src/main/java/org/aerogear/mobile/example/ui/AuthFragment.java
+++ example/src/main/java/org/aerogear/mobile/example/ui/AuthFragment.java
@@ -1,14 +1,21 @@
 package org.aerogear.mobile.example.ui;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.aerogear.mobile.auth.AuthService;
 import org.aerogear.mobile.auth.authenticator.DefaultAuthenticateOptions;
 import org.aerogear.mobile.auth.user.UserPrincipal;
 import org.aerogear.mobile.core.Callback;
 import org.aerogear.mobile.example.R;
+import org.aerogear.mobile.security.SecurityService;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -40,24 +47,32 @@ public class AuthFragment extends BaseFragment {
     public void doLogin() {
         AuthService authService = ((MainActivity) getActivity()).getAuthService();
         DefaultAuthenticateOptions authOptions =
-                        new DefaultAuthenticateOptions(this.getActivity(), LOGIN_RESULT_CODE);
-        try {
-            authService.login(authOptions, new Callback<UserPrincipal>() {
-                @Override
-                public void onSuccess(UserPrincipal models) {
-                    // user logged in, continue on..
-                    Log.i(TAG, "user logged in " + models.toString());
-                    ((MainActivity) getActivity()).navigateToAuthDetailsView(models);
-                }
+            new DefaultAuthenticateOptions(this.getActivity(), LOGIN_RESULT_CODE);
+        authService.login(authOptions, new Callback<UserPrincipal>() {
+            @Override
+            public void onSuccess(UserPrincipal models) {
+                // user logged in, continue on..
+                Log.i(TAG, "user logged in " + models.toString());
+                ((MainActivity) getActivity()).navigateToAuthDetailsView(models);
+            }
 
-                @Override
-                public void onError(Throwable error) {
-                    // there is an error during the login
-                    Log.e(TAG, "login failed due to error " + error.getLocalizedMessage());
-                }
-            });
-        } catch (IllegalStateException e) {
-            Log.e("Pinning Error", e.getMessage());
-        }
+            @Override
+            public void onError(Throwable error) {
+                // there is an error during the login
+                Log.e(TAG, "login failed due to error " + error.getLocalizedMessage());
+                failureDialog(error.getLocalizedMessage());
+            }
+        });
+    }
+
+    public void failureDialog(String e) {
+        String error = e;
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder certPinningErrorBuilder = new AlertDialog.Builder(activity);
+                certPinningErrorBuilder.setTitle("Failed to Authenticate").setMessage(error).show().create();
+            }
+        });
     }
 }
