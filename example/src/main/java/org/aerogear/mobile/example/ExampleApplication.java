864943bbfffda2277c3233efa9e29e7b4722a39e
Ali Ok <aliok@apache.org>
2018-05-09 15:14:19 +0300

    AEROGEAR-2669 - Fix compile error on example app

--- example/src/main/java/org/aerogear/mobile/example/ExampleApplication.java
+++ example/src/main/java/org/aerogear/mobile/example/ExampleApplication.java
@@ -2,15 +2,7 @@ package org.aerogear.mobile.example;
 
 import android.app.Application;
 
-import org.aerogear.mobile.core.MobileCore;
 
 public class ExampleApplication extends Application {
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-
-        MobileCore.getInstance().destroy();
-    }
 
 }
