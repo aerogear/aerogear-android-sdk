be52fdcd1aeb21de626b73c0d3ad7da98e0c9adc
rachael-oregan <roregan@redhat.com>
2018-05-22 11:34:24 +0100

    AEROGEAR-2784 remove example app

--- example/src/main/java/org/aerogear/mobile/example/ExampleApplication.java
+++
@@ -1,8 +0,0 @@
-package org.aerogear.mobile.example;
-
-import android.app.Application;
-
-
-public class ExampleApplication extends Application {
-
-}
--- example/src/main/java/org/aerogear/mobile/example/ui/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package org.aerogear.mobile.example.ui;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Base class for all activities that want to work with AeroGearServices SDK
- */
-public abstract class BaseActivity extends AppCompatActivity {
-}
