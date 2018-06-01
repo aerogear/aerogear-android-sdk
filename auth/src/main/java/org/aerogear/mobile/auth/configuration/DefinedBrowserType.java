fe3aa755f185666b58e6acf3c522a712398adf00
rachael-oregan <roregan@redhat.com>
2018-06-01 12:45:05 +0100

    remove parentheses in enum

--- auth/src/main/java/org/aerogear/mobile/auth/configuration/DefinedBrowserType.java
+++ auth/src/main/java/org/aerogear/mobile/auth/configuration/DefinedBrowserType.java
@@ -7,7 +7,7 @@ public enum DefinedBrowserType {
     /**
      * This represents a Google Chrome browser where the custom tab and versions can be specified.
      */
-    CHROME(),
+    CHROME,
     /**
      * This represents a Mozilla Firefox browser where custom tab and versions can be specified.
      */
