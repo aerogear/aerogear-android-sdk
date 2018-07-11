8bd9cf6176dbceca8d2481fd7e90b97bd08ca318
Daniel Passos <daniel@passos.me>
2018-07-11 09:58:37 -0300

    Add SyncService

--- sync/src/test/java/org/aerogear/mobile/sync/ExampleUnitTest.java
+++ sync/src/test/java/org/aerogear/mobile/sync/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package org.aerogear.mobile.sync;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
